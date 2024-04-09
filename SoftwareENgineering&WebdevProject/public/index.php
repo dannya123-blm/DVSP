f9cb71ddc3d046d8f72f51a2b4f7d8638c456f46
Slooma951 <b00149614@mytudublin.ie>
2024-04-09 04:22:22 +0100

    updated the index and added product page php

--- SoftwareENgineering&WebdevProject/public/index.php
+++ SoftwareENgineering&WebdevProject/public/index.php
@@ -5,4 +5,9 @@ require '../template/header.php';
 <link rel="stylesheet" href="../css/style.css">
 <main>
     <section class="banner" style="background-image: url('../images/banner.png');"
+    <div class="bannercontainer">
+        <h1>Looking for something?</h1>
+        <p>Shop our wide range of electronics and<br> accessories at unbeatable prices!</p>
+        <a href="productpage.php" class="productbutton">Shop Now</a>
+    </div>
 </main>
