387005a17fd32fcafd8c84571d0616d7bb11b623
Slooma951 <b00149614@mytudublin.ie>
2024-04-09 04:43:45 +0100

    added images worked more on index and started the css

--- SoftwareENgineering&WebdevProject/public/index.php
+++ SoftwareENgineering&WebdevProject/public/index.php
@@ -12,14 +12,16 @@ require '../template/header.php';
 
 <body>
 <main>
-
     <section class="banner" style="background-image: url('../images/banner.png');">
-    <div class="bannercontainer">
-        <h1>Looking for something?</h1>
-        <p>Shop our wide range of electronics and<br> accessories at unbeatable prices!</p>
-        <a href="productpage.php" class="productbutton">Shop Now</a>
-    </div>
+        <div class="bannercontainer">
+            <div class="bannercontent">
+                <h1>Looking for something?</h1>
+                <p>Shop our wide range of electronics and<br> accessories at unbeatable prices!</p>
+                <a href="productpage.php" class="productbutton">Shop Now</a>
+            </div>
+        </div>
     </section>
+
 
 
 
