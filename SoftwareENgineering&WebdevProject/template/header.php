924aa5e55042283303ee65d0d5060bc8cc756204
dannya123-blm <B00150058@mytudublin.ie>
2024-04-09 04:59:27 +0100

    tt

--- SoftwareENgineering&WebdevProject/template/header.php
+++ SoftwareENgineering&WebdevProject/template/header.php
@@ -30,7 +30,7 @@
             </div>
             <div>
                 <a href="cart.php"><img src="../images/cart.png" alt="Basket"></a>
-                <div class="cartBuutton">Cart</div>
+                <div class="cartButton">Cart</div>
             </div>
         </div>
         <div class="searchBarButton">
@@ -39,3 +39,27 @@
         </div>
     </div>
 </header>
+
+<section class="hero">
+    <h1>Welcome to DVS Expansion</h1>
+    <p>Discover our latest products and deals.</p>
+    <button class="button">Explore</button>
+</section>
+
+<footer>
+    <div class="social">
+        <ul>
+            <li><a href="#"><img src="facebook-icon.png" alt="Facebook"></a></li>
+            <li><a href="#"><img src="twitter-icon.png" alt="Twitter"></a></li>
+            <li><a href="#"><img src="instagram-icon.png" alt="Instagram"></a></li>
+        </ul>
+    </div>
+    <div class="footer-links">
+        <a href="#" class="footer-link">About Us</a>
+        <a href="#" class="footer-link">Contact Us</a>
+        <a href="#" class="footer-link">Privacy Policy</a>
+    </div>
+</footer>
+
+</body>
+</html>
