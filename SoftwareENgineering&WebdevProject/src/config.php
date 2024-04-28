592e968f6ca3e13e0e135e9d0edfe14b8a181185
dannya123-blm <B00150058@mytudublin.ie>
2024-04-28 11:32:42 +0100

    Update config.php

--- SoftwareENgineering&WebdevProject/src/config.php
+++ SoftwareENgineering&WebdevProject/src/config.php
@@ -5,9 +5,10 @@
  */
 $host = "localhost";
 $username = "root";
-$password = 'Yxng$alem951';
-$dbname = "dvsdb";
+$password = 'root';
+$dbname = "dvsdb"; // Define your database name here
 $dsn = "mysql:host=$host;dbname=$dbname";
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 );
+?>
